1181443cf96a568e3052b9f1038bf383430d5b8e
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-07-27 16:18:25 -0300

    feat: implemented firebase
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ src/configs/firebase.ts
@@ -0,0 +1,35 @@
+
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env';
+
+import { getApps, getApp, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
+}
+
+let app;
+
+if (getApps().length <= 0) app = initializeApp(config);
+else app = getApp();
+
+export default app;
+export const auth = getAuth();
+export const storage = getStorage();
+export const firestore = getFirestore();
